375d878e44385664ab1964ebdfcd921b8654da49
Phann Phearoen <phannphearin2@gmail.com>
2022-05-08 15:33:32 +0700

    config user to authenticatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def self.authenticate(email, password)
+    user = User.find_for_authentication(email: email)
+    user&.valid_password?(password) ? user : nil
+  end
+  def self.get_writer_by_id writer_id
+    self.where(id: writer_id).last
+  end
+  def as_json(options = {})
+    super(options.merge({ except: [:created_at, :is_deleted, :uuid, :updated_at] }))
+  end
+  
+  protected
+  def confirmation_required?
+    false
+  end
 end
