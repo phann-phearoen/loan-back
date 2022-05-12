18a1abcf873d34a739734f67f0c4a2bcaf5edb78
Phann Phearoen <phannphearin2@gmail.com>
2022-05-12 22:39:54 +0700

    serve deposit

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,24 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :doorkeeper_authorize!, only: [:index]
+  before_action :doorkeeper_authorize!, only: [:index, :deposit]
+  before_action :get_user
 
   def index
     users = User.all
   end
+  def deposit
+    if @me.is_admin
+      user = User.find(params[:id])
+      user.total_deposit += params[:new_deposit]
+      if user.save
+        render json: 'ប្រត្តិបត្តិការណ៍ដាក់ប្រាក់សន្សំជោគជ័យ។'
+      else
+        render staus: 500
+      end
+    end
+  end
+
+  private
+    def get_user
+      @me = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       get 'users', to: 'users#index'
+      put 'users/deposit', to: 'users#deposit'
     end
   end
 end
