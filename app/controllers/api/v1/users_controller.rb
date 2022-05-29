0af52085ceafa98979c5f8e0c54fe512e826c009
Phann Phearoen <phannphearin2@gmail.com>
2022-05-29 10:31:49 +0700

    make all depositable accounts from members table

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,17 +5,6 @@ class Api::V1::UsersController < ApplicationController
   def index
     users = User.all
   end
-  def deposit
-    if @me.is_admin
-      user = User.find(params[:id])
-      user.total_deposit += params[:new_deposit]
-      if user.save
-        render json: 'ប្រត្តិបត្តិការណ៍ដាក់ប្រាក់សន្សំជោគជ័យ។'
-      else
-        render staus: 500
-      end
-    end
-  end
   def get_all_users
     users = User.where(is_member: true)
     render json: users
