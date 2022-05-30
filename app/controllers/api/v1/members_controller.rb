67e3bce0856aff8c870fe03818093ee9b9587bb6
Phann Phearoen <phannphearin2@gmail.com>
2022-05-30 08:56:37 +0700

    correctly serve memebers/index & get_member_by_name

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -1,24 +1,34 @@
 class Api::V1::MembersController < ApplicationController
   before_action :doorkeeper_authorize!, only: [:index, :deposit]
-  before_action :get_member, only: [:deposit]
+  before_action :get_member, only: [:deposit, :view_member]
   def index
-    members = Member.all
+    admin_id = params[:admin_id]
+    members = Member.where.not(id: admin_id)
     render json: members
   end
   def deposit
-    @member.total_deposit += params[:new_deposit]
-    if @member.save
-      render json: 'ប្រត្តិបត្តិការណ៍ដាក់ប្រាក់សន្សំជោគជ័យ។'
+    transaction_params = {
+      amount: params[:amount].to_f,
+      type: 'deposit',
+      member_id: params[:member_id]
+    }
+    commit = Transaction.commit_transaction transaction_params
+    @member.total_deposit += params[:amount].to_f
+    if @member.save && commit
+      render json: {
+        transaction: commit,
+        message: 'ប្រត្តិបត្តិការណ៍ដាក់ប្រាក់សន្សំជោគជ័យ។'
+      }
     else
       render staus: 500
     end
   end
   def get_member_by_name
-    member = Member.where(name: params[:name])
+    member = Member.where(name: params[:admin_name]).last
     if member
       render json: member
     else
-      render status: 404
+      render 404
     end
   end
   private
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,16 @@
 class Transaction < ApplicationRecord
   belongs_to :member
+
+  def self.commit_transaction transaction_params
+    transaction = Transaction.new(
+      amount: transaction_params[:amount],
+      type: transaction_params[:type],
+      member_id: transaction_params[:member_id]
+    )
+    if transaction.save
+      transaction
+    else
+      "error"
+    end
+  end
 end
