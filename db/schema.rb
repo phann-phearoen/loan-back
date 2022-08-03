7a89250dba3828f2e697007d1286d67d073473ff
Phann Phearoen <phannphearin2@gmail.com>
2022-08-03 10:34:14 +0700

    remove roles from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_19_041408) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_03_033159) do
   create_table "agreements", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -93,9 +93,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_19_041408) do
     t.string "national_id", null: false
     t.float "total_deposit", limit: 53, default: 0.0, null: false
     t.string "loan", default: "0"
-    t.boolean "is_admin"
-    t.boolean "is_member"
-    t.boolean "is_client"
     t.string "phone", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
