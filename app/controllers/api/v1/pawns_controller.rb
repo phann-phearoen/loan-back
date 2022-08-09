1b74bab3a4727f4aee0a650c58070b6c9e447d4b
Phann Phearoen <phannphearin2@gmail.com>
2022-08-09 15:17:49 +0700

    pass params to create new pawn inidvidually

--- app/controllers/api/v1/pawns_controller.rb
+++ app/controllers/api/v1/pawns_controller.rb
@@ -2,12 +2,26 @@ class Api::V1::PawnsController < ApplicationController
   before_action :doorkeeper_authorize!, only: [:new_pawn]
 
   def new_pawn
-    pawn_hash = params
-    pawn = Pawn.create_new_pawn pawn_hash
-    if pawn
-      render json: pawn
-    else
-      render status 500
-    end
+    number          = params[:no]
+    registered_date = params[:registered_date]
+    registered_by   = params[:registered_by]
+    surface_area    = params[:surface_area]
+    north           = params[:north]
+    south           = params[:south]
+    east            = params[:east]
+    west            = params[:west]
+    other_assets    = params[:other_assets]
+    pawn = Pawn.create_new_pawn(
+      number,
+      registered_date,
+      registered_by,
+      surface_area,
+      north,
+      south,
+      west,
+      east,
+      other_assets
+    )
+    render json: pawn
   end
 end
--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,18 +1,26 @@
 class Pawn < ApplicationRecord
   belongs_to :loan
-  def self.create_new_pawn pawn_hash
-    pawn = Pawn.new 
-    pawn.number = pawn_hash[:no]
-    pawn.registered_date = pawn_hash[:registered_date]
-    pawn.registered_by = pawn_hash[:registered_by]
-    pawn.surface_area = pawn_hash[:surface_area]
-    pawn.north = pawn_hash[:north]
-    pawn.east = pawn_hash[:east]
-    pawn.south = pawn_hash[:south]
-    pawn.west = pawn_hash[:west]
-    other_assets = pawn_hash[:other_assets]
-    if pawn.save
-      pawn
-    end
+  def self.create_new_pawn(
+    number,
+    registered_date,
+    registered_by,
+    surface_area,
+    north,
+    south,
+    west,
+    east,
+    other_assets
+  )
+    pawn = Pawn.new
+    pawn.number = number
+    pawn.registered_date = registered_date
+    pawn.registered_by = registered_by
+    pawn.surface_area = surface_area
+    pawn.north = north
+    pawn.east = east
+    pawn.south = south
+    pawn.west = west
+    other_assets = other_assets
+    pawn.save
   end
 end
